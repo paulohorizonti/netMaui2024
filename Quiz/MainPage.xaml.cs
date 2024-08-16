b869830a376ce8820d10b9c0128d24eb0e88c37b
tupaerp <tupaerp@gmail.com>
2024-08-16 17:44:40 -0300

    Primeiro commit

---
+++ Quiz/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Quiz.Paginas;
+
+namespace Quiz
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            SecureStorage.Default.RemoveAll();
+        }
+
+
+        private async void BtnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string pergunta = await DisplayPromptAsync("Pergunta", "Qual é seu nome? ", "OK", "Cancelar");
+
+            await SecureStorage.Default.SetAsync("nome", pergunta);
+
+            await Navigation.PushAsync(new Questao1());
+        }
+    }
+
+}
